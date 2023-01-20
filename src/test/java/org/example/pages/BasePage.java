d923def857ddcde175cfc739561e64b670c12c7f
nadaS90 <engnada_45@outlook.com>
2023-01-20 17:47:44 +0200

    Add Hooks and BasePage

---
+++ src/test/java/org/example/pages/BasePage.java
@@ -0,0 +1,25 @@
+package org.example.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+
+public class BasePage
+{
+    static WebDriver driver;
+
+    public BasePage(WebDriver driver)
+    {
+        this.driver = driver;
+    }
+
+    public static void ClickBtn(WebElement button)
+    {
+        button.click();
+    }
+
+    public static void SendTxt(WebElement textElm , String value)
+    {
+        textElm.sendKeys(value);
+    }
+}
