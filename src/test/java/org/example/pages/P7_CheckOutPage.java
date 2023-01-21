e075dd9893a0589f638ff06c32a4871cc7a9a6f4
nadaS90 <engnada_45@outlook.com>
2023-01-21 06:31:30 +0200

    Add item to the cart

---
+++ src/test/java/org/example/pages/P7_CheckOutPage.java
@@ -0,0 +1,30 @@
+package org.example.pages;
+
+import org.example.stepDefinition.Hooks;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class P7_CheckOutPage extends BasePage
+{
+    public P7_CheckOutPage(WebDriver driver)
+    {
+        super(driver);
+    }
+//    public static final String userNameTxtBoxID ="loginusername";
+//
+//    public WebElement userNameTextBox = Hooks.driver.findElement(By.id(userNameTxtBoxID));
+//
+//    public void UserEnterUserName(String userName)
+//    {
+//        SendTxt(userNameTextBox, userName);
+//    }
+//    public void UserEnterPassword(String password)
+//    {
+//        SendTxt(passwordTxtBox, password);
+//    }
+//    public void UserClickSignInBtn()
+//    {
+//        ClickBtn(loginBtn);
+//    }
+}
